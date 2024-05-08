e776994d4d6ee8ae26b443aff75c969d4af98877
edumarce03 <eduardomarce2003@gmail.com>
2024-05-08 01:04:25 -0500

    Componente Navbar: v.1

--- HuellaVerde/src/app/app.component.ts
+++ HuellaVerde/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/basics/navbar/navbar.component';
+import { FooterComponent } from './components/basics/footer/footer.component';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'HuellaVerde';
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
--- HuellaVerde/src/app/app.routes.ts
+++ HuellaVerde/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MaterialComponent } from './components/material/material.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'material', component: MaterialComponent },
+  { path: 'calculator', component: CalculatorComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+];
