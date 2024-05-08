a8616e6d1b08fe91b774c801525d376f7c783cd7
edumarce03 <eduardomarce2003@gmail.com>
2024-05-08 01:21:43 -0500

    Configuración Tipografía: Google Fonts

--- HuellaVerde/tailwind.config.js
+++ HuellaVerde/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
