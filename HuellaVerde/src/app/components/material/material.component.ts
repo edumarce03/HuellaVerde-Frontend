dc63fba4bca6daad66952361739dd9d8355121cf
edumarce03 <eduardomarce2003@gmail.com>
2024-05-09 12:25:21 -0500

    Componente Recursos y Materiales

--- HuellaVerde/src/app/components/material/material.component.ts
+++ HuellaVerde/src/app/components/material/material.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-material',
   standalone: true,
   imports: [],
   templateUrl: './material.component.html',
-  styleUrl: './material.component.css'
+  styleUrl: './material.component.css',
 })
-export class MaterialComponent {
-
+export class MaterialComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {
+    AOS.init();
+  }
 }
